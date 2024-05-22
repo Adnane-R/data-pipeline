de2e56d268a057e91ceb1b1ab9e9534066eba43d
Rachidi Adnane <ad97.rach@gmail.com>
2024-05-22 09:23:55 +0100

    init

---
+++ dags/hello.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'refaverama',
+    'start_date': datetime(2024,03,04),
+    'catchup': False
+}
+
+
+dag = DAG(
+    'First_K8s',
+    default_args = default_args,
+    schedule = timedelta(days=1)
+)
+
+t1 = BashOperator(
+    task_id = "first_K8s",
+    bash_command = 'echo "first_k8s"',
+    dag = dag
+)
